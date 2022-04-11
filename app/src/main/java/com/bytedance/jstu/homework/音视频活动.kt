5fd2b0b78dc3cd5bf681be60c37fb069f3c99c05
satgo1546 <satgo1546@yeah.net>
2022-04-11 20:34:45 +0800

    横屏继续全屏播放

--- app/src/main/java/com/bytedance/jstu/homework/音视频活动.kt
+++ app/src/main/java/com/bytedance/jstu/homework/音视频活动.kt
@@ -1,7 +1,10 @@
 package com.bytedance.jstu.homework
 
+import android.content.res.Configuration
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.MediaController
 import android.widget.SeekBar
 import android.widget.VideoView
@@ -17,5 +20,25 @@ class 音视频活动 : AppCompatActivity() {
 			setAnchorView(视频视图)
 		})
 		视频视图.start()
+	}
+
+	override fun onConfigurationChanged(newConfig: Configuration) {
+		super.onConfigurationChanged(newConfig)
+		if (newConfig.orientation == ORIENTATION_LANDSCAPE) {
+			// 从官方文档贴进来就开幕雷击，Google搞什么名堂？非要破坏API兼容性？
+			window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+				// Set the content to appear under the system bars so that the
+				// content doesn't resize when the system bars hide and show.
+				or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+				or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+				// Hide the nav bar and status bar
+				or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+				or View.SYSTEM_UI_FLAG_FULLSCREEN)
+		} else {
+			window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+				or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+		}
 	}
 }
